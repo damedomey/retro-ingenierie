b5cc2444865c21c57a83a6b9ad7ca483799ae468
Theophile-Yvars <79571637+Theophile-Yvars@users.noreply.github.com>
2024-01-04 18:44:41 +0100

    remove unuse file

--- outils/analyse_microservices/analyse_microservices.py
+++
@@ -1,6 +0,0 @@
-class Analyse_microservices():
-    def __init__(self, token_github):
-        print("Analyse microservices")
-        self.token_github = token_github
-
-    def run(self, repository_github):
