f92e55a24a90bdb9eea66c0f362103727e37ab34
Theophile-Yvars <79571637+Theophile-Yvars@users.noreply.github.com>
2024-01-08 17:19:23 +0100

    update

--- outils/utils/db_analyser/src/db_analyser_naive.py
+++ outils/utils/db_analyser/src/db_analyser_naive.py
@@ -1,7 +1,7 @@
 import yaml
 
+from outils.utils.db_analyser.src.csv_manager import CSV_Manager
 from utils.Colors import Couleurs
-from outils.analyse_microservices.utils.db_analyser.src.csv_manager import CSV_Manager
 
 class DB_Analyser_Naive():
     def __init__(self):
